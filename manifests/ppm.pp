8d33ceb99dad3974b8fcfebfad1bedd72bb27537
Dave Mangot <github@mangot.com>
2013-05-12 00:04:52 -0700

    got a working puppetmaster up and running

--- manifests/ppm.pp
+++ manifests/ppm.pp
@@ -1,14 +1,19 @@
-yumrepo { "puppetlabs-products":
-	enabled  => 1, 
-	name => "Puppet_Products",
-	descr => "Puppet_Products",
-	baseurl => "http://yum.puppetlabs.com/el/6Server/products/x86_64/",
+Package { require => Exec['puppetyum-setup'] }
+
+# need to fix this so it handles 5 vs. 6 repos transparently, split operatingsystemrelease?
+exec { 'puppetyum-setup':
+  command => '/usr/bin/rpm -ivh http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm',
+  creates => '/etc/yum.repos.d/puppetlabs.repo'
 }
 
-yumrepo { "puppetlabs-dependencies":
-	enabled  => 1, 
-	name => "Puppet_Dependencies",
-	descr => "Puppet_Dependencies",
-	baseurl => "http://yum.puppetlabs.com/el/6Server/dependencies/x86_64/",
-}
+package { 'puppet-server':
+  ensure => latest,
+  }
+
+service { 'puppetmaster':
+  ensure  => running,
+  enable  => true,
+  require => Package['puppet-server'],
+  }
+
 
